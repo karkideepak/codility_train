ffe9cec5f7e63f5623ec11b5543a234609ae3006
Deepak <102180983+karkideepak@users.noreply.github.com>
2026-05-10 22:50:36 -0500

    Add examples and syntax for for loops in Python

---
+++ for loop.py
@@ -0,0 +1,14 @@
+1.1: For loop syntax
+for some_variable in range_of_values:
+  loop_body
+
+
+for i in range(0,100):
+  print(i)
+
+for i in range(100):
+  print i
+
+factorial = 1
+for i in range (1, n+1):
+  factorial *= 1
